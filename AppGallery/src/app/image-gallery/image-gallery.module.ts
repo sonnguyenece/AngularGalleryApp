96e28b949d466ed347d78edd9a0f34164be6f381
sonnguyenece@gmail.com <sonnguyenece@gmail.com>
2020-07-10 11:02:54 +0700

    init

---
+++ AppGallery/src/app/image-gallery/image-gallery.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ImageCardComponent } from './image-card/image-card.component';
+
+
+
+@NgModule({
+  declarations: [ImageCardComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ImageGalleryModule { }
